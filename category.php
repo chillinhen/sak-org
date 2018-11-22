69d62dd3915e7bfdb2e8c5f28a899acf9f9feaeb
chillinhen <c.hillen@gmx.net>
2018-11-22 16:19:12 +0100

    remove-meta-info-from-loop

--- category.php
+++ category.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
+<div class="site-content">
 
+		<?php if (have_posts()): ?>
+				<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+				<?php while (have_posts()) : the_post(); ?>
+					<div class="main-content">
+						<?php get_template_part('loop'); ?>
+						<?php get_template_part('pagination'); ?>
+					</div>
+				<?php
+				endwhile;
+				wp_reset_postdata();
+				?>
+
+
+		<?php endif; ?>
+
+</div>
 	<main role="main">
 		<!-- section -->
 		<section>
