f1d4fd05f06e6619e4693382c6fe9d2bdfa3b5fa
chillinhen <c.hillen@gmx.net>
2018-12-09 11:16:19 +0100

    update suppages.php

--- partials/subpages.php
+++ partials/subpages.php
@@ -12,13 +12,13 @@ $subLoop = new WP_Query($filter);
 if ($subLoop->found_posts >= 3) :
     #echo '<h1>passt</h1>';
     $columnGrid = ' subpanels';
-else : 
+else :
     #echo '<h1>passt nicht</h1>';
     $columnGrid = ' smallpanels';
 endif;
 if ($subLoop->have_posts()) : ?>
 <div class="<?php echo $selection;echo $columnGrid ;?>">
-    <?php while ($subLoop->have_posts()) : $subLoop->the_post(); 
+    <?php while ($subLoop->have_posts()) : $subLoop->the_post();
             if ($selection == 'panels') :
                 get_template_part('partials/panels', 'thumbnails');
             elseif ($selection == 'accordion') :
@@ -27,5 +27,3 @@ if ($subLoop->have_posts()) : ?>
     endwhile; wp_reset_postdata(); ?>
 </div>
 <?php endif; ?>
-
-
